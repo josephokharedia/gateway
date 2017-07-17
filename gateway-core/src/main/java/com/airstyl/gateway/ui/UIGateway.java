f553f9558eb8445734db9ef21daf93edbbaf11ab
JOSEPH OKHAREDIA <JOSEPHOKHAREDIA@PERAGO.COM>
2017-07-17 16:47:19 +0200

    Improved

--- gateway-core/src/main/java/com/airstyl/gateway/ui/UIGateway.java
+++ gateway-core/src/main/java/com/airstyl/gateway/ui/UIGateway.java
@@ -1,14 +1,25 @@
 package com.airstyl.gateway.ui;
 
 
+import com.airstyl.be360.api.IFramework;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 @Path("ui")
 public class UIGateway {
 
+    @Inject
+    private IFramework framework;
+
     @GET
-    public String greet() {
-        return "Hello";
+    @Path("{id}")
+    public Response getView(@PathParam("id") String id) {
+
+        String result = framework.getView(id);
+        return Response.status(200).entity(result).build();
     }
 }
